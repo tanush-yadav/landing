2889bc3cd33dda553540446c6d6214f00f2626da
Tanush <143377537+tanushmavy@users.noreply.github.com>
2025-08-29 11:21:15 +0530

    feat: posthog added through wizard

--- next.config.js
+++ next.config.js
@@ -11,9 +11,20 @@ const nextConfig = {
   },
   compress: true,
   poweredByHeader: false,
-  // experimental: {
-  //   optimizeCss: true,
-  // },
-}
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
